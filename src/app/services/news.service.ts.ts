06391d149be522b5623f86c9c7ea8c58be712b97
william <williampereira21@gmail.com>
2025-11-10 16:54:18 -0300

    fix calendar, service news open vercel

--- src/app/services/news.service.ts.ts
+++ src/app/services/news.service.ts.ts
@@ -1,24 +1,29 @@
-import { Injectable } from '@angular/core';
+import { Injectable, isDevMode } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class NewsService {
   private apiKey = '026fbb7f2981493ebfe52849126572cb';
-  private apiUrl = '/newsapi/v2/everything';
+
+  // Se estiver em desenvolvimento → usa proxy local
+  // Caso contrário → usa URL completa da NewsAPI
+  private apiUrl = isDevMode()
+    ? '/newsapi/v2/everything'
+    : 'https://newsapi.org/v2/everything';
 
   constructor(private http: HttpClient) {}
 
   getNews(q: string, from: string): Observable<any> {
-    let params = new HttpParams()
+    const params = new HttpParams()
       .set('q', q)
       .set('from', from)
       .set('language', 'pt')
       .set('sortBy', 'publishedAt')
       .set('apiKey', this.apiKey);
 
-    console.log('Fazendo requisição para:', `${this.apiUrl}?${params.toString()}`);
-    
+    console.log('📡 Requisição para:', `${this.apiUrl}?${params.toString()}`);
+
     return this.http.get(this.apiUrl, { params });
   }
 }
